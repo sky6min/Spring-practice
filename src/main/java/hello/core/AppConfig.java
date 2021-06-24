9b3c7d18ec1d7477789c6c3adfd90c1cdba8aada
sky6min <sky6min@gmail.com>
2021-06-24 19:31:23 +0900

    configuration anotation

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -15,18 +15,24 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
+    //@Bean memberService -> new MemoryMemberRepository()
+    //@Bean orderService -> new MemoryMemberRepository()
+
     @Bean
     public MemberService memberService() {
+        System.out.println(" call AppConfig.memberService");
         return new MemberServiceImpl(MemberRepository());
     }
 
     @Bean
     public MemberRepository MemberRepository() {
+        System.out.println(" call AppConfig.MemberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService() {
+        System.out.println(" call AppConfig.orderService");
         return new OrderServiceImpl(MemberRepository(), discountPolicy());
     }
 
