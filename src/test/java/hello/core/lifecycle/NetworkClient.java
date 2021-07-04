3d31a71b327f3d5a715dc666fe5fca94b9102a14
sky6min <sky6min@gmail.com>
2021-07-04 21:44:03 +0900

    lifeCycle

---
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -0,0 +1,57 @@
+package hello.core.lifecycle;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class NetworkClient {// implements InitializingBean, DisposableBean
+
+    private String url;
+
+    public NetworkClient() {
+        System.out.println("생성자 호출, url = " + url);
+
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    // 서비스 시작시 호출
+    public void connect() {
+        System.out.println("connect = " + url);
+    }
+
+
+    public void call(String message) {
+        System.out.println("call: " + url + "message = " + message);
+    }
+
+    //서비스 종료시 호출
+    public void disconnect() {
+        System.out.println("close: " + url);
+    }
+
+//    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메세지");
+    }
+
+    @PostConstruct
+    public void init() {
+        System.out.println("NetworkClient.init");
+        connect();
+        call("초기화 연결 메세지");
+    }
+
+//    @Override
+    @PreDestroy
+    public void close() {
+        System.out.println("NetworkClient.close");
+        disconnect();
+    }
+}
